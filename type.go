dfcff3ad4e763f222555435dad2132bb47e7cb0e
je09 <je09@yandex.ru>
2025-02-21 15:15:46 +0500

    upd(spotifind): refactor interfaces

--- type.go
+++ type.go
@@ -1,5 +1,15 @@
 package spotifind
 
+type Spotifinder interface {
+	Connector
+	Search
+}
+
+type Connector interface {
+	Reconnect(configAuth SpotifindAuth) error
+	Continue(ch SpotifindChan, p ProgressChan) error
+}
+
 type Search interface {
 	SearchPlaylistAllMarkets(ch SpotifindChan, p ProgressChan, q, ignore []string) error
 	SearchPlaylistForMarket(ch SpotifindChan, p ProgressChan, market string, q, ignore []string) error
